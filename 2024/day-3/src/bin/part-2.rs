e724732ae7e5ce19ceb4f4a2ef33f8e519ac9d0d
sakresh <sarkeshnandu@gmail.com>
2024-12-08 13:02:43 +0530

    Part-2 of day-3 is completed

--- 2024/day-3/src/bin/part-2.rs
+++ 2024/day-3/src/bin/part-2.rs
@@ -9,21 +9,31 @@ fn compute(input: &str) -> u32{
     let mut res: Vec<u32>;
     let regular_expression = regex::Regex::new(r"\d+").unwrap();
     let mut sum: u32 = 0;
+    let mut flag = true;
     for i in 0..contents.len(){
-        res = regular_expression.
-            find_iter(contents[i]).
-            map(|x| x.as_str()).
-            map(|x| x.parse::<u32>().unwrap()).
-            collect();
-        sum += res.iter().product::<u32>();
+        if contents[i] == "don\'t()"{
+            flag = false;
+            continue;
+        }
+        if contents[i] == "do()"{
+            flag = true;
+            continue;
+        }
+        if flag{
+            res = regular_expression.
+                find_iter(contents[i]).
+                map(|x| x.as_str()).
+                map(|x| x.parse::<u32>().unwrap()).
+                collect();
+            sum += res.iter().product::<u32>();
+        }
     }
     sum
 }
 
 fn contains(input: &str) -> Vec<&str>{
     let res: Vec<&str>;
-    // Regular expression for part-2 has to be written
-    let regular_expression = regex::Regex::new(r"mul\(\d+,\s*\d+\)").unwrap();
+    let regular_expression = regex::Regex::new(r"(mul\(([0-9]+),([0-9]+)\)|do\(\)|don't\(\))").unwrap();
     res = regular_expression.
         find_iter(input).
         map(|x| x.as_str()).
@@ -32,23 +42,23 @@ fn contains(input: &str) -> Vec<&str>{
 }
 
 fn main() -> io::Result<()>{
-    let path = "inputs/input-1.txt";
-    println!("{:?}", compute(path));
+    let path = "inputs/input-2.txt";
+    println!("{}", compute(path));
     Ok(())
 }
 
-#[cfg(test)]
 mod test{
+#[cfg(test)]
     use super::*;
 
     #[test]
     fn check1(){
-        assert_eq!(compute("inputs/sample.txt"), 161);
+        assert_eq!(compute("inputs/sample-2.txt"), 48);
     }
 
     #[test]
     fn check2(){
-        assert_eq!(compute("inputs/input-1.txt"), 166630675);
+        assert_eq!(compute("inputs/input-2.txt"), 93465710);
     }
 
 }
